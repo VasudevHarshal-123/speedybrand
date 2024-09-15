53c7998cdc687c318e0d1e7706351f547b8012e1
Harshal <harshal@Harshals-MacBook-Pro.local>
2024-09-15 15:30:32 +0530

    Added project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { CssBaseline } from '@mui/material';
+import DashboardLayout from "./components/DashboardLayout";
+import RevenueByCountryChart from "./components/RevenueByCountryChart";
+import MauVsDauChart from "./components/MAUvsDAUChart";
+import DataTable from "./components/DataTable";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CssBaseline />
+      <DashboardLayout/>
+      <DataTable/>
+      <RevenueByCountryChart/>
+      <MauVsDauChart/>
     </div>
   );
 }
